462e505d0e2f41286ee040af96e59f74f1f45172
Smiti Shakya <smitishk12@gmail.com>
2020-04-02 14:14:46 +0545

    megamenu intial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var overlay = document.getElementById('overlay');
+var closeMenu = document.getElementById('close-menu');
+
+document.getElementById('open-menu').addEventListener('click', function(){
+    overlay.classList.add('show-menu');
+});
